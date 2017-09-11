d6878ed742c666c4c04decfca9b6c47260aef778
Chika <djchikalkin@gmail.com>
2017-09-11 13:23:59 +0300

    Initial

---
+++ bot.py
@@ -0,0 +1,89 @@
+import telebot
+
+import sheets
+
+bot = telebot.TeleBot ("419654586:AAGl98vEWE0iY9xXhnnwHygJ6bq7jwakQDY")
+
+now_week = sheets.now_week
+
+
+def log(message, answer):
+    print ("\n-----------------------------")
+    from datetime import datetime
+    print ("[{0}]".format (datetime.now ()))
+    print ("User - {0} {1}. (id = {2}) \n\nMessage - {3} \n\nAnswer - {4}".format (message.from_user.first_name,
+                                                                                   message.from_user.last_name,
+                                                                                   str (message.from_user.id),
+                                                                                   message.text,
+                                                                                   answer))
+
+
+@bot.message_handler (commands=['about'])
+def handle_text(message):
+    bot.send_message (message.chat.id,
+                      "Schedule Bot by Chika.\nNow only for 647M RSREU group\nhttps://vk.com/chikalkin")
+    send_days_keyboard (message)
+
+
+def send_days_keyboard(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup (True, False)
+    user_markup.row ('Понедельник', 'Вторник')
+    user_markup.row ('Среда', 'Четверг')
+    user_markup.row ('Пятница')
+    answer = "Выбери день...\n_(Сегодня {0}, если что)_".format (sheets.today)
+    bot.send_message (message.from_user.id, answer, parse_mode="Markdown", reply_markup=user_markup)
+
+
+def send_week_keyboard(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup (True, False)
+    user_markup.row ('Числитель', 'Знаменатель')
+    answer = "Выбери неделю...\n_(Сейчас {0}, если что)_".format (now_week)
+    bot.send_message (message.from_user.id, answer, parse_mode="Markdown", reply_markup=user_markup)
+
+
+def send_hide_keyboard(message):
+    hide_markup = telebot.types.ReplyKeyboardRemove ()
+    bot.send_message (message.from_user.id, "...", reply_markup=hide_markup)
+
+
+@bot.message_handler (commands=["start"])
+def handle_text(message):
+    send_days_keyboard (message)
+
+
+day = ""
+
+
+@bot.message_handler (content_types=["text"])
+def handle_text(message):
+    if message.text == "Пятница" or message.text == "Суббота":
+        # bot.send_chat_action (message.chat.id, 'typing')
+        answer = "В этот день нет пар. Отдыхай :)"
+        bot.send_message (message.chat.id, answer)
+        send_days_keyboard (message)
+
+    elif message.text == "Воскресенье":
+        answer = "Совсем чтоль поехавший? В воскресенье не бывает пар."
+        bot.send_message (message.chat.id, answer, parse_mode="Markdown")
+        send_days_keyboard (message)
+
+
+    elif message.text == "Понедельник" or message.text == "Вторник" or \
+                    message.text == "Среда" or message.text == 'Четверг':
+        bot.day = message.text
+        send_week_keyboard (message)
+
+    elif message.text == "Числитель" or message.text == "Знаменатель":
+        week = message.text
+        bot.send_chat_action (message.chat.id, 'typing')
+        answer = "*Пара* #1\n" + sheets.get_first_lesson (bot.day, week)
+        bot.send_message (message.chat.id, answer, parse_mode="Markdown")
+        bot.send_chat_action (message.chat.id, 'typing')
+        answer = "*Пара* #2\n" + sheets.get_second_lesson (bot.day, week)
+        bot.send_message (message.chat.id, answer, parse_mode="Markdown")
+        send_days_keyboard (message)
+
+        # send_days_keyboard (message)
+
+
+bot.polling (none_stop=True, interval=0)
---
+++ sheets.py
@@ -0,0 +1,47 @@
+import datetime
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name ('googleauth.json', scope)
+client = gspread.authorize (creds)
+
+sheet = client.open ("Sheet1").sheet1
+
+dayofweek = datetime.datetime.today ().weekday ()
+# dayofweek = 1
+day_list = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница"]
+today = day_list[dayofweek]
+# today = "Понедельник"
+now_week = sheet.cell (23, 2).value
+
+
+# nowweek = "Числитель"
+
+
+def get_row(day, num, week):
+    days = sheet.findall (day)
+    for d in days:
+        row = d.row
+        if sheet.cell (row, 2).value == week:
+            if sheet.cell (row, 3).value == num:
+                return row
+
+
+def print_lesson(row):
+    return "*Время:* " + sheet.cell (row, 4).value + "\n" \
+           "*Ауд.:* " + sheet.cell (row, 5).value + "\n" + \
+           "*Тип:* " + sheet.cell (row, 6).value + "\n" + \
+           "*Назв.:* " + sheet.cell (row, 7).value + "\n" + \
+           "*Препод:* " + sheet.cell (row, 8).value
+
+
+def get_first_lesson(day, week):
+    row = get_row (day, "1", week)
+    return print_lesson (row)
+
+
+def get_second_lesson(day, week):
+    row = get_row (day, "2", week)
+    return print_lesson (row)
